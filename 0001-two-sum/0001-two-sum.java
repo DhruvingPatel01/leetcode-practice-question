5dc36240ed8a7465b4c8af572d0549eeb85cf01e
DhruvingPatel01 <2403031080035@paruluniversity.ac.in>
2026-06-09 12:43:26 +0530

    Time: 2 ms (99.41%), Space: 47.3 MB (23.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,25 +1,17 @@
 import java.util.HashMap;
 import java.util.Map;
 
-class Solution {
+public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // Create a map to store the value and its index
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            
-            // Check if the required complement exists in the map
-            if (map.containsKey(complement)) {
-                return new int[] { map.get(complement), i };
-            }
-            
-            // Store the current number and its index for future lookups
-            map.put(nums[i], i);
-        }
-        
-        // Per constraints, a solution always exists, but we return 
-        // an empty array or throw an error to satisfy the compiler.
-        return new int[] {};
-    }
-}
+            Map<Integer, Integer> seen = new HashMap<>();
+                    for (int i = 0; i < nums.length; i++) {
+                                int complement = target - nums[i];
+                                            if (seen.containsKey(complement)) {
+                                                            return new int[] { seen.get(complement), i };
+                                                                        }
+                                                                                    seen.put(nums[i], i);
+                                                                                            }
+                                                                                                    return new int[0];
+                                                                                                        }
+                                                                                                        }
+                                                                                                        
