5bb3a207934165493306cd36a15d7fdb55d73fb5
DhruvingPatel01 <2403031080035@paruluniversity.ac.in>
2026-07-06 23:21:27 +0530

    Time: 1 ms (96.86%), Space: 44.3 MB (89.34%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
@@ -1,30 +1,41 @@
-import java.util.*;
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        // Use LinkedList for result to make addFirst() O(1)
-        LinkedList<List<Integer>> result = new LinkedList<>();
+        List<List<Integer>> result = new ArrayList<>();
         if (root == null) return result;
 
         Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
+        queue.offer(root);
 
         while (!queue.isEmpty()) {
-            int levelSize = queue.size();
-            List<Integer> currentLevel = new ArrayList<>();
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
 
-            for (int i = 0; i < levelSize; i++) {
+            for (int i = 0; i < size; i++) {
                 TreeNode node = queue.poll();
-                currentLevel.add(node.val);
+                level.add(node.val);
 
-                if (node.left != null) queue.add(node.left);
-                if (node.right != null) queue.add(node.right);
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
             }
-            
-            // Key Step: Always add the new level to the beginning of the list
-            result.addFirst(currentLevel);
+            result.add(level);
         }
 
+        Collections.reverse(result);
         return result;
     }
 }
