b833f20fdf888b8585e0c6579b6cfe7b1602f44a
DhruvingPatel01 <2403031080035@paruluniversity.ac.in>
2026-07-22 23:03:21 +0530

    Time: 0 ms (100%), Space: 44.7 MB (32.93%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,35 +1,29 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
-        int i = n - 2;
-
-        // 1. Find the first decreasing element from the right
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
+        int i = n-2;
+        while(i >= 0 && nums[i] >= nums[i+1]) {
             i--;
         }
-
-        if (i >= 0) {
-            // 2. Find the element to swap with nums[i]
-            int j = n - 1;
-            while (nums[j] <= nums[i]) {
+        if(i>= 0 ){
+            int j = n -1;
+            while(nums[j] <= nums[i]) {
                 j--;
             }
-            swap(nums, i, j);
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
         }
-
-        // 3. Reverse the elements after the pivot index i
-        reverse(nums, i + 1, n - 1);
+        reverse(nums,i+1,n-1);
     }
+    private void reverse(int[] nums, int left,int right) {
+        while(left < right){
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
 
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-
-    private void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            swap(nums, start++, end--);
+            left++;
+            right--;
         }
     }
 }
