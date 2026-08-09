787419fd98ffb15cfd59370f4d8a018cf70b62d6
DhruvingPatel01 <2403031080035@paruluniversity.ac.in>
2026-08-09 22:50:58 +0530

    Time: 2 ms (96.3%), Space: 45.3 MB (96.52%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,29 +1,23 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        backtrack(candidates, target, 0, new ArrayList<>(), result);
-        return result;
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrack(candidates, target, 0, new ArrayList<>(), ans);
+        return ans;
     }
-
-    private void backtrack(int[] nums, int remain, int start, List<Integer> current, List<List<Integer>> result) {
-        if (remain == 0) {
-           
-            result.add(new ArrayList<>(current));
+   
+    void backtrack(int[] candidates, int target, int index,
+                   List<Integer> list, List<List<Integer>> ans) {
+        if(target == 0) {
+            ans.add(new ArrayList<>(list));
             return;
         }
-        
-        for (int i = start; i < nums.length; i++) {
-       
-            if (nums[i] <= remain) {
-                current.add(nums[i]);
-                
-                backtrack(nums, remain - nums[i], i, current, result);
-   
-                current.remove(current.size() - 1);
-            }
+        if(target < 0) {
+            return;
+        }
+        for(int i = index;i<candidates.length;i++) {
+            list.add(candidates[i]);
+            backtrack(candidates,target - candidates[i],i,list,ans);
+            list.remove(list.size()- 1);
         }
     }
 }
