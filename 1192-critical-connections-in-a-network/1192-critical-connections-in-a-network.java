b1e4309a2d2abeb942a730f45cac1dcaf5f1e24d
DhruvingPatel01 <2403031080035@paruluniversity.ac.in>
2026-09-02 15:58:59 +0530

    Time: 86 ms (67.56%), Space: 271.4 MB (13.93%) - LeetHub

---
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
@@ -0,0 +1,72 @@
+class Solution {
+    static int timer;
+    static List<List<Integer>> adj;
+    static int[] disc;
+    static int[] low;
+    static List<List<Integer>> bridges;
+            public List<List<Integer>>criticalConnections(int n, List<List<Integer>> connections) {
+                adj= new ArrayList<>();
+                for (int i = 0; i<n; i++) {
+                adj.add(new ArrayList<>());
+            }
+        for (List<Integer> edge :connections) {
+        int u=edge.get(0);
+        int v=edge.get(1);
+        adj.get(u).add(v);
+        adj.get(v).add(u);
+        }
+        disc = new int[n];
+        low = new int[n];
+        Arrays.fill(disc, -1);
+        Arrays.fill(low, -1);
+         disc = new int[n];
+        low = new int[n];
+
+        Arrays.fill(disc, -1);
+        Arrays.fill(low, -1);
+
+        bridges = new ArrayList<>();
+
+        timer = 0;
+
+        
+        for (int i = 0; i < n; i++) {
+
+            if (disc[i] == -1) {
+                dfs(i, -1);
+            }
+        }
+
+        return bridges;
+
+}
+static void dfs(int u, int parent) {
+
+        disc[u] = low[u] = timer++;
+
+        for (int v : adj.get(u)) {
+
+            
+            if (v == parent)
+                continue;
+
+            
+            if (disc[v] == -1) {
+
+                dfs(v, u);
+
+                
+                low[u] = Math.min(low[u], low[v]);
+
+                
+                if (low[v] > disc[u]) {
+                    bridges.add(Arrays.asList(u, v));
+                }
+            }
+            else {
+               
+                low[u] = Math.min(low[u], disc[v]);
+            }
+        }
+}
+}
